d8a05454ae6abc8bdd58f643a2369be706b957cd
Manish Rakaskoti <helloworld00ff@gmail.com>
2025-07-08 11:21:54 +0545

    Oauth setting update

--- social_login/settings.py
+++ social_login/settings.py
@@ -113,14 +113,18 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
         # For each OAuth based provider, either add a ``SocialApp``
         # (``socialaccount`` app) containing the required client
         # credentials, or list them here:
         'APP': {
-            'client_id': '123',
-            'secret': '456',
+            'client_id': os.getenv('GOOGLE_CLIENT_ID'),
+            'secret': os.getenv('GOOGLE_CLIENT_SECRET'),
             'key': ''
         }
     }
